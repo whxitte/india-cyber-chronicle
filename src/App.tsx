47fdb5563a4a511c1f21b7e7a085e7c97da53811
whxitte <hemker444@gmail.com>
2025-09-25 11:55:30 +0530

    Fix GitHub Pages routing and add 404 handling

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/india-cyber-chronicle">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/browse" element={<Browse />} />
